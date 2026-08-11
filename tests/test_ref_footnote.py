e546d7ec98c7e4131c7ca9fc98913d61e1e29608
Michael R. Bernstein <zopemaven@gmail.com>
2026-08-10 18:57:09 -0600

    feat: implement ref and footnote inline templates for HTML5 and XHTML

---
+++ tests/test_ref_footnote.py
@@ -0,0 +1,44 @@
+"""Tests for ref and footnote inline node rendering in HTML5 and XHTML."""
+from asciidoctype import AsciiDoctypeRenderer
+
+
+def test_render_ref_link_html5():
+    renderer = AsciiDoctypeRenderer(target_format="html5")
+    node = {
+        "name": "ref",
+        "type": "inline",
+        "variant": "link",
+        "target": "https://asciidoc.org",
+        "inlines": [{"name": "text", "type": "string", "value": "AsciiDoc Site"}],
+    }
+    output = renderer.render(node)
+    assert '<a href="https://asciidoc.org">AsciiDoc Site</a>' in output
+
+
+def test_render_ref_xref_xhtml():
+    renderer = AsciiDoctypeRenderer(target_format="xhtml")
+    node = {
+        "name": "ref",
+        "type": "inline",
+        "variant": "xref",
+        "target": "#section-1",
+        "inlines": [{"name": "text", "type": "string", "value": "Section 1"}],
+    }
+    output = renderer.render(node)
+    assert '<a href="#section-1" class="xref">Section 1</a>' in output
+
+
+def test_render_footnote_html5():
+    renderer = AsciiDoctypeRenderer(target_format="html5")
+    node = {
+        "name": "footnote",
+        "type": "inline",
+        "id": "fn-1",
+        "number": 1,
+        "value": "Footnote text.",
+    }
+    output = renderer.render(node)
+    expected_footnote = (
+        '<sup class="footnote" id="_footnote_fn-1"><a href="#_footnotedef_1">1</a></sup>'
+    )
+    assert expected_footnote in output
