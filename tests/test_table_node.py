45ac5c31b04cbbf9f20ca02cbd55fb1cec4c33db
Michael R. Bernstein <zopemaven@gmail.com>
2026-08-25 07:25:19 -0600

    feat: add colgroup and cell alignment support to HTML5 and XHTML table templates

--- tests/test_table_node.py
+++ tests/test_table_node.py
@@ -8,7 +8,7 @@ def test_render_table_html5():
     node = {
         "name": "table",
         "type": "block",
-        "attributes": {"id": "table-1", "options": "header"},
+        "attributes": {"id": "table-1", "options": "header", "cols": "1,3"},
         "title": [{"name": "text", "type": "string", "value": "Data Table"}],
         "rows": [
             {
@@ -18,11 +18,14 @@ def test_render_table_html5():
                     {
                         "name": "cell",
                         "type": "block",
+                        "halign": "left",
+                        "valign": "top",
                         "inlines": [{"name": "text", "type": "string", "value": "Col 1"}],
                     },
                     {
                         "name": "cell",
                         "type": "block",
+                        "halign": "right",
                         "inlines": [{"name": "text", "type": "string", "value": "Col 2"}],
                     },
                 ],
@@ -45,6 +48,7 @@ def test_render_table_html5():
                     {
                         "name": "cell",
                         "type": "block",
+                        "halign": "center",
                         "blocks": [
                             {
                                 "name": "paragraph",
@@ -61,10 +65,15 @@ def test_render_table_html5():
     assert 'class="tableblock"' in output
     assert 'id="table-1"' in output
     assert "Data Table" in output
+    assert "<colgroup>" in output
+    assert '<col style="width: 25%;" />' in output or '<col style="width: 25%;">' in output
+    assert '<col style="width: 75%;" />' in output or '<col style="width: 75%;">' in output
     assert "<thead>" in output
-    assert "<th>Col 1</th>" in output
+    assert '<th class="halign-left valign-top">Col 1</th>' in output
+    assert '<th class="halign-right">Col 2</th>' in output
     assert "<tbody>" in output
     assert "<td><p>Cell 1</p></td>" in output
+    assert '<td class="halign-center"><p>Cell 2</p></td>' in output
 
 
 def test_render_table_xhtml():
@@ -72,6 +81,7 @@ def test_render_table_xhtml():
     node = {
         "name": "table",
         "type": "block",
+        "attributes": {"cols": "20%,80%"},
         "rows": [
             {
                 "name": "row",
@@ -80,15 +90,87 @@ def test_render_table_xhtml():
                     {
                         "name": "cell",
                         "type": "block",
+                        "halign": "center",
                         "inlines": [{"name": "text", "type": "string", "value": "Value 1"}],
-                    }
+                    },
+                    {
+                        "name": "cell",
+                        "type": "block",
+                        "inlines": [{"name": "text", "type": "string", "value": "Value 2"}],
+                    },
                 ],
             }
         ],
     }
     output = renderer.render(node)
     assert 'class="tableblock"' in output
-    assert "<td>Value 1</td>" in output
+    assert "<colgroup>" in output
+    assert '<col style="width: 20%;" />' in output or '<col style="width: 20%;">' in output
+    assert '<col style="width: 80%;" />' in output or '<col style="width: 80%;">' in output
+    assert '<td class="halign-center">Value 1</td>' in output
+    assert "<td>Value 2</td>" in output
+
+
+def test_render_table_cell_roles_and_alignments():
+    for fmt in ("html5", "xhtml"):
+        renderer = AsciiDoctypeRenderer(target_format=fmt)
+        node = {
+            "name": "table",
+            "type": "block",
+            "attributes": {"cols": "1,1"},
+            "rows": [
+                {
+                    "name": "row",
+                    "type": "block",
+                    "cells": [
+                        {
+                            "name": "cell",
+                            "type": "block",
+                            "halign": "center",
+                            "valign": "middle",
+                            "attributes": {"role": "custom-cell"},
+                            "inlines": [
+                                {"name": "text", "type": "string", "value": "Special Cell"}
+                            ],
+                        },
+                        {
+                            "name": "cell",
+                            "type": "block",
+                            "attributes": {"role": "plain-cell"},
+                            "inlines": [{"name": "text", "type": "string", "value": "Normal Cell"}],
+                        },
+                    ],
+                }
+            ],
+        }
+        output = renderer.render(node)
+        assert 'class="halign-center valign-middle custom-cell"' in output
+        assert 'class="plain-cell"' in output
+
+
+def test_render_table_no_colgroup():
+    for fmt in ("html5", "xhtml"):
+        renderer = AsciiDoctypeRenderer(target_format=fmt)
+        node = {
+            "name": "table",
+            "type": "block",
+            "rows": [
+                {
+                    "name": "row",
+                    "type": "block",
+                    "cells": [
+                        {
+                            "name": "cell",
+                            "type": "block",
+                            "inlines": [{"name": "text", "type": "string", "value": "Simple Cell"}],
+                        }
+                    ],
+                }
+            ],
+        }
+        output = renderer.render(node)
+        assert "<colgroup>" not in output
+        assert "<td>Simple Cell</td>" in output
 
 
 def test_extract_col_widths():
